667b9c12c6ba66769334d60fc1dd60f9de0ddce4
carolluckmann <carolinaluckmann05@gmail.com>
2024-04-01 14:47:55 -0300

    criação do template para Acessorios

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,8 @@ import router from './router';
 <template>
   <header>
     <router-link :to="{ name: 'home' }">Home</router-link> |
-    <router-link to="/categorias">Categorias</router-link> 
+    <router-link to="/categorias">Categorias</router-link> |
+    <router-link to="/acessorios">Acessorios</router-link> |
   </header>
   <main>
     <RouterView />
